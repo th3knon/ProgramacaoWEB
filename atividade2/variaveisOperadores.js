037b2bcfdb0e546119d9b3d7e8d6bc4a1e0b0d0f
GabriellMatias <gabriel.goiasat@gmail.com>
2023-08-16 10:07:04 -0300

    :hammer: Feat: Add atividade2 folder

---
+++ atividade2/variaveisOperadores.js
@@ -0,0 +1,24 @@
+  // Declaração das variáveis numéricas
+let num_A = 10;
+let num_B = 5;
+
+// Adição
+let resultadoSoma = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B}: ${resultadoSoma}`);
+
+// Subtração
+let resultadoSubtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B}: ${resultadoSubtracao}`);
+
+// Multiplicação
+let resultadoMultiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B}: ${resultadoMultiplicacao}`);
+
+// Divisão
+let resultadoDivisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B}: ${resultadoDivisao}`);
+
+// Módulo (Resto da divisão)
+let resultadoModulo = num_A % num_B;
+console.log(`Resultado do módulo entre ${num_A} e ${num_B}: ${resultadoModulo}`);
+
