c5df1c249de8c1a99c516a98ae4404d0433ed4bd
icaro vinnicius <91756830+th3knon@users.noreply.github.com>
2023-10-25 10:26:37 -0300

    Update exercicio1.js

--- atividade2/exercicio1.js
+++ atividade2/exercicio1.js
@@ -1,24 +1,24 @@
   // Declaração das variáveis numéricas
-let num_A = 10;
+let num_A = 20;
 let num_B = 5;
 
 // Adição
 let resultadoSoma = num_A + num_B;
-console.log(`Resultado da adição entre ${num_A} e ${num_B}: ${resultadoSoma}`);
+console.log(`O resultado da adição entre ${num_A} e ${num_B}: ${resultadoSoma}`);
 
 // Subtração
 let resultadoSubtracao = num_A - num_B;
-console.log(`Resultado da subtração entre ${num_A} e ${num_B}: ${resultadoSubtracao}`);
+console.log(`O resultado da subtração entre ${num_A} e ${num_B}: ${resultadoSubtracao}`);
 
 // Multiplicação
 let resultadoMultiplicacao = num_A * num_B;
-console.log(`Resultado da multiplicação entre ${num_A} e ${num_B}: ${resultadoMultiplicacao}`);
+console.log(`O resultado da multiplicação entre ${num_A} e ${num_B}: ${resultadoMultiplicacao}`);
 
 // Divisão
 let resultadoDivisao = num_A / num_B;
-console.log(`Resultado da divisão entre ${num_A} e ${num_B}: ${resultadoDivisao}`);
+console.log(`O resultado da divisão entre ${num_A} e ${num_B}: ${resultadoDivisao}`);
 
 // Módulo (Resto da divisão)
 let resultadoModulo = num_A % num_B;
-console.log(`Resultado do módulo entre ${num_A} e ${num_B}: ${resultadoModulo}`);
+console.log(`O resultado do módulo entre ${num_A} e ${num_B}: ${resultadoModulo}`);
 
